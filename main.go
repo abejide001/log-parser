bfbeb891f073c1e8a1d98e3914091ee59a2164be
abejidefemi1@gmail.com <abejidefemi1@gmail.com>
2019-09-30 23:43:41 +0100

    refactor code

--- main.go
+++ main.go
@@ -3,30 +3,53 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	var domains []string
-	var sum map[string]int
-	sum = make(map[string]int)
-	in := bufio.NewScanner(os.Stdin)
-	for in.Scan() {
-		fields := strings.Fields(in.Text())
-		fmt.Printf("domain: %s - visits: %s\n", fields[0], fields[1])
-		domain := fields[0]
-		visits, _ := strconv.Atoi(fields[1])
-		sum[domain] += visits
-		_, ok := sum[domain]
-		if !ok {
-			domains = append(domains, domain)
+	total := 0
+
+	var (
+		domains []string // to store unique domain names
+		sum     map[string]int
+	)
+	sum = make(map[string]int) // create a map to store the domain name
+
+	in := bufio.NewScanner(os.Stdin) // get the input
+
+	for in.Scan() { // loop through to get the text
+		fields := strings.Fields(in.Text()) // split the line
+		if len(fields) != 2 {
+			log.Panic("fields must be two")
+			return
 		}
+
+		domain := fields[0]                    // the domain name
+		visits, err := strconv.Atoi(fields[1]) // the number of visits
+		if visits < 0 || err != nil {
+			log.Panic("wrong input")
+			return
+		}
+		_, ok := sum[domain] // check if there is a domain
+
+		if !ok {
+			domains = append(domains, domain) // add domain to the slice
+		}
+		sum[domain] += visits
 	}
-	fmt.Printf("%-30s, %10s\n", "DOMAIN", "VISITS")
+
+	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS") // headers
+	fmt.Println(strings.Repeat("-", 45))           // print number of lines
+
+	for _, domain := range domains {
+		visits := sum[domain]
+		total += visits
+		fmt.Printf("%-30s %10d\n", domain, visits)
+	}
 	fmt.Println(strings.Repeat("-", 45))
-	for domain, visits := range sum {
-		fmt.Printf("%-30s, %10d\n", domain, visits)
-	}
+
+	fmt.Printf("%-30s %10d\n", "TOTAL", total)
 }
